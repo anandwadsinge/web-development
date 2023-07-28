7c409a22ef1173dcab1ff75d1bbf20e59f62071d
Anand Wadsinge <Anand@MacBook-Air.local>
2023-07-28 23:24:18 +0530

    Modified

--- 4. Conditional Statement/script.js
+++ 4. Conditional Statement/script.js
@@ -1,2 +1,34 @@
 //if-else statements
 
+// const age = 17;
+
+// if (age > 18) {
+//     console.log('Eligible to vote');
+// } else {
+//     console.log('Not eligible to vote');
+// }
+
+
+
+//else if statement
+
+// const marks = 99;
+
+// if (marks > 35 && marks <= 40) {
+//     console.log('Just Passed');
+// } else if (marks > 40 && marks <= 60) {
+//     console.log('Average');
+// } else if (marks > 60 && marks <= 75 ) {
+//     console.log('Second Class');
+// } else if (marks > 75 && marks <= 90) {
+//     console.log('First Class');
+// } else {
+//     console.log('Distinction');
+// }
+
+
+//Ternary Operator
+
+const marks = 35;
+const result = marks >= 35 ? "Passed" : "Failed"
+console.log(`You are ${result}`);
